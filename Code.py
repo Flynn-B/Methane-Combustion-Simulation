ae4e1ff3ff88353ffca1c5f1d2e6dfab25f03019
Flynn-B <98112947+Flynn-B@users.noreply.github.com>
2022-01-27 09:28:22 -0700

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,4 +1,4 @@
-molecules = []
+molecules = [] #List that holds all the dictionaries of molecules of data
 canvas_size = {'x':1000,'y':600}
 
 debug = False #True for debug mode 
@@ -6,16 +6,16 @@ debug = False #True for debug mode
 #Space Partioning collsion needs to be added
 
 def setup():  # setup() runs once
-    size(canvas_size['x'], canvas_size['y'])
-    frameRate(30)
+    size(canvas_size['x'], canvas_size['y']) #Sets canvas size based on predetermined value
+    frameRate(30) #Sets framrate
     
-    if debug == False:
+    if debug == False: #Runs code as normal if debug is False
         for i in range(115):
-            molecule = {'x':random(0,canvas_size['x']),'y':random(0,canvas_size['y']),'vel':PVector(random(-5,5),random(-5,5)),'color':255,'radius':random(10,20), 'density':random(4,6), 'mass':0}
-            molecule['mass'] = pow((molecule['radius'] * 4/3 * PI),3) * molecule['density']
+            molecule = {'x':random(0,canvas_size['x']),'y':random(0,canvas_size['y']),'vel':PVector(random(-5,5),random(-5,5)),'color':255,'radius':random(10,20), 'density':random(4,6), 'mass':0} #Assigns dictionary 'molecule' values
+            molecule['mass'] = pow((molecule['radius'] * 4/3 * PI),3) * molecule['density'] #Calculates 'mass' based on 'density' and 'size'
             molecule['color']-=20*molecule['density']
-            molecules.append(molecule)
-    elif debug == True:
+            molecules.append(molecule) #Adds the dictionary of the 'molecule' to the list of 'molecules'
+    elif debug == True: #Debug mode for code, creates head on collision between two circles
         molecule = {'x':0,'y':canvas_size['y']/2,'vel':PVector(5,0),'color':255,'radius':random(10,20), 'density':random(4,6), 'mass':0}
         molecule['mass'] = pow((molecule['radius'] * 4/3 * PI),3) * molecule['density']
         molecules.append(molecule)
@@ -24,23 +24,21 @@ def setup():  # setup() runs once
         molecules.append(molecule)
         
 
-def draw():
+def draw(): #Function Draws Molecules to Canvas
     background(204)
     for molecule in molecules[:]: 
-        fill(255,molecule['color'],molecule['color'])
-        ellipse(molecule['x'],molecule['y'],molecule['radius'],molecule['radius'])
-        molecule['x']+=molecule['vel'].x
-        molecule['y']+=molecule['vel'].y
+        fill(255,molecule['color'],molecule['color']) #Colors Molecules based on assigned 'color' value
+        ellipse(molecule['x'],molecule['y'],molecule['radius'],molecule['radius']) #Draws Ellipses 
+        molecule['x']+=molecule['vel'].x #Moves Molecule's x cordinate based on vel.x
+        molecule['y']+=molecule['vel'].y #Moves Molecule's y cordinate based on vel.y
         wall_collide(molecule) #Calls wall colliode function
         collision_detection(molecule) #Calls molecule collsion detection
 
 def collision_physics(object1, object2): #Elastic (No energy lossed) collision
-    initial_vel1 = object1['vel']
-    initial_vel2 = object2['vel']
-    object2['vel']= ((2*object1['mass'])/(object1['mass']+object2['mass']) * initial_vel1) - (((object1['mass']-object2['mass'])/(object1['mass']+object2['mass']))*initial_vel2)
-    object1['vel']= (((object1['mass']-object2['mass'])/(object1['mass']+object2['mass']))*initial_vel1) +((2*object2['mass'])/(object1['mass']+object2['mass']) * initial_vel2)
-    #pass
-    #m_{1} v_{1 i}+m_{2} v_{2 i}=m_{1} v_{1 f}+m_{2} v_{2 f}
+    initial_vel1 = object1['vel'] #Records initial velocity of object 1 for physics calculations
+    initial_vel2 = object2['vel']#Records initial velocity of object 2 for physics calculations
+    object2['vel']= ((2*object1['mass'])/(object1['mass']+object2['mass']) * initial_vel1) - (((object1['mass']-object2['mass'])/(object1['mass']+object2['mass']))*initial_vel2) #Calcs object2 velocity based on collision and mass
+    object1['vel']= (((object1['mass']-object2['mass'])/(object1['mass']+object2['mass']))*initial_vel1) +((2*object2['mass'])/(object1['mass']+object2['mass']) * initial_vel2)#Calcs object1 velocity based on collision and mass
     
 
 def collision_detection(object):
